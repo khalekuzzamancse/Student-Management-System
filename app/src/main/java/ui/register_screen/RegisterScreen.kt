8a7f42f4c7ad807dd1abde4de60027aa97b041a1
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-06-27 13:56:45 +0600

    Dropdown menu added

--- app/src/main/java/ui/register_screen/RegisterScreen.kt
+++ app/src/main/java/ui/register_screen/RegisterScreen.kt
@@ -28,33 +28,28 @@ fun TextualForm() {
             label = "Name",
             onTextChanged = {},
             icon = Icons.Default.Person,
-            modifier = Modifier
         )
         FormInputField(
             label = "Email",
             onTextChanged = {},
             icon = Icons.Default.Email,
             keyboardType = KeyboardType.Email,
-            modifier = Modifier
         )
         FormInputField(
             label = "Phone",
             onTextChanged = {},
             icon = Icons.Default.Phone,
             keyboardType = KeyboardType.Phone,
-            modifier = Modifier
         )
         FormInputField(
             label = "Blood Group",
             onTextChanged = {},
             icon = Icons.Default.Bloodtype,
-            modifier = Modifier,
         )
         FormInputField(
             label = "Gender",
             onTextChanged = {},
             icon = Icons.Default.Man,
-            modifier = Modifier,
         )
         //use visual transfer for date of birth
         //use the date picker for date of birth
@@ -64,34 +59,32 @@ fun TextualForm() {
             icon = Icons.Default.DateRange,
             keyboardType = KeyboardType.Number,
             visualTransformation = DateVisualTransformer,
-            modifier = Modifier
         )
         //as a student
         FormInputField(
             label = "Father Name",
             onTextChanged = {},
             icon = Icons.Default.Person,
-            modifier = Modifier,
         )
         FormInputField(
             label = "Mother Name",
             onTextChanged = {},
             icon = Icons.Default.Person,
-            modifier = Modifier,
+
         )
         FormInputField(
             label = "Father Phone No",
             onTextChanged = {},
             icon = Icons.Default.Phone,
             keyboardType = KeyboardType.Phone,
-            modifier = Modifier
+
         )
         FormInputField(
             label = "Mother Phone No",
             onTextChanged = {},
             icon = Icons.Default.Phone,
             keyboardType = KeyboardType.Phone,
-            modifier = Modifier
+
         )
         //For teacher
         FormInputField(
@@ -99,7 +92,6 @@ fun TextualForm() {
             onTextChanged = {},
             icon = Icons.Default.Info,
             keyboardType = KeyboardType.Phone,
-            modifier = Modifier
         )
         ExposedDropdownMenu(
             items = districtList,
