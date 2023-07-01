020e00460eafc4a7aca9f4fd77f33c8d9a1739d3
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-07-01 11:48:34 +0600

    Custom Button Added

--- app/src/main/java/ui/dashboard/Dashboard.kt
+++ app/src/main/java/ui/dashboard/Dashboard.kt
@@ -2,13 +2,37 @@ package ui.dashboard
 
 import android.util.Log
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Book
 import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Schedule
 import androidx.compose.material.icons.filled.VerifiedUser
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-
+/*
+Problems:
+I want to check,if the
+ */
+@Composable
+fun AdminDashboard() {
+    val list = listOf(
+        DashboardItem("Edit Profile Request", Icons.Default.Edit),
+        DashboardItem("Delete Profile Request", Icons.Default.Book),
+        DashboardItem("Register  Request", Icons.Default.Book),
+        DashboardItem("Add Exam", Icons.Default.Book),
+        DashboardItem("Up Coming Exam", Icons.Default.Schedule),
+        DashboardItem("Previous Exam", Icons.Default.Schedule),
+        DashboardItem("Publish Result", Icons.Default.Schedule),
+        DashboardItem("Search User", Icons.Default.VerifiedUser),
+        DashboardItem("Add Course", Icons.Default.Add),
+    )
+    DashBoard(
+        list = list,
+        onDashboardItemClick = {
+            Log.i("Dashboard", it)
+        }
+    )
+}
 @Composable
 fun StudentDashboard() {
     val list = listOf(
@@ -25,9 +49,18 @@ fun StudentDashboard() {
         })
 }
 
-@Preview
+
+@Preview(
+    showSystemUi = true
+)
+@Composable
+fun AdminDashboardAdmin() {
+    AdminDashboard()
+}
+@Preview(
+    showSystemUi = true
+)
 @Composable
 fun StudentDashboardPreview() {
-
     StudentDashboard()
 }
--- app/src/main/java/ui/dashboard/DashboardItem.kt
+++ app/src/main/java/ui/dashboard/DashboardItem.kt
@@ -1,20 +1,16 @@
 package ui.dashboard
 
-import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Book
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.filled.Schedule
-import androidx.compose.material.icons.filled.VerifiedUser
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -22,8 +18,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 data class DashboardItem(
@@ -31,6 +27,14 @@ data class DashboardItem(
     val icon: ImageVector,
 )
 
+/*
+         In this item
+         Only one line of text is can be present
+         if the more text then it will not shown
+         as a result all the card will have the same height
+         later we have to modify such that:
+         all the child takes the same height as it tallest siblibg
+          */
 
 @Composable
 fun DashBoard(
@@ -38,7 +42,7 @@ fun DashBoard(
     list: List<DashboardItem>,
 ) {
     LazyVerticalGrid(
-        columns = GridCells.Adaptive(150.dp),
+        columns = GridCells.Fixed(2),
         contentPadding = PaddingValues(16.dp),
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp),
@@ -65,22 +69,44 @@ fun DashboardItem(
 ) {
     Surface(
         modifier = modifier
-            .fillMaxWidth()
-            .padding(16.dp)
             .clickable { onDashboardItemClick(label) },
         tonalElevation = 5.dp,
         shadowElevation = 5.dp
     ) {
         Column(
-            modifier = Modifier.padding(16.dp),
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(16.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
         ) {
             Icon(
                 imageVector = icon,
                 contentDescription = null
             )
-            Text(text = label)
+            /*
+            In this item
+            Only one line of text is can be present
+            if the more text then it will not shown
+            as a result all the card will have the same height
+            later we have to modify such that:
+            all the child takes the same height as it tallest siblibg
+             */
+            Text(
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
+                text = label
+            )
         }
     }
+}
+
+@Preview
+@Composable
+fun DashboardItemPreview() {
+    DashboardItem(
+        label = "Edit Profile Request",
+        icon = Icons.Default.Edit,
+        onDashboardItemClick = {}
+    )
 }
 
