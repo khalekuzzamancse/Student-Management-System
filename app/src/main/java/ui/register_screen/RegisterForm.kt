ef165dc9aeb6f17bc3a8e88fa0584caaf99dedbc
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-07-03 21:12:13 +0600

    Drawing Board updated

--- app/src/main/java/ui/register_screen/RegisterForm.kt
+++ app/src/main/java/ui/register_screen/RegisterForm.kt
@@ -20,8 +20,11 @@ import androidx.compose.material.icons.filled.LocationCity
 import androidx.compose.material.icons.filled.Man
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Phone
+import androidx.compose.material.icons.filled.Upload
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,11 +32,13 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.GoogleMap
+import ui.drawing_board.WhiteBoard
 
 /*
 Form
@@ -82,132 +87,147 @@ fun RegistrationForm(
     var showGoogleMap by remember {
         mutableStateOf(false)
     }
+    var showDrawingBoard by remember {
+        mutableStateOf(false)
+    }
     var location by remember { mutableStateOf("") }
     Box(modifier = modifier) {
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .verticalScroll(
-                    rememberScrollState()
-                ),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            FormInputField(
-                label = "Name",
-                onTextChanged = {},
-                icon = Icons.Default.Person,
-            )
-            FormInputField(
-                label = "Email",
-                onTextChanged = {},
-                icon = Icons.Default.Email,
-                keyboardType = KeyboardType.Email,
-            )
-            FormInputField(
-                label = "Phone",
-                onTextChanged = {},
-                icon = Icons.Default.Phone,
-                keyboardType = KeyboardType.Phone,
-            )
-            ExposedDropdownMenu(
-                items = FormData.bloodGroupList,
-                onItemSelected = {
-                },
-                label = "Blood Group",
-                icon = Icons.Default.Bloodtype
-            )
-            ExposedDropdownMenu(
-                items = FormData.genderList,
-                onItemSelected = {
-                },
-                label = "Gender",
-                icon = Icons.Default.Man
-            )
-            //017
-            //use visual transfer for date of birth
-            //use the date picker for date of birth
-            CustomDatePicker(
-                label = "Date of Birth",
-                icon = Icons.Default.DateRange,
-            )
-            ExposedDropdownMenu(
-                items = FormData.registrationType,
-                onItemSelected = {
-                    registerAsStudent = it == FormData.registrationType[0]
-                    registerAsTeacher = it == FormData.registrationType[1]
-                },
-                label = "Register As",
-                icon = Icons.Default.AppRegistration
-            )
 
-            if (registerAsStudent) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .verticalScroll(
+                        rememberScrollState()
+                    ),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
                 FormInputField(
-                    label = "Father Name",
-                    onTextChanged = {
-
-                    },
-                    icon = Icons.Default.Person,
-                )
-                FormInputField(
-                    label = "Mother Name",
+                    label = "Name",
                     onTextChanged = {},
                     icon = Icons.Default.Person,
                 )
                 FormInputField(
-                    label = "Father Phone No",
+                    label = "Email",
                     onTextChanged = {},
-                    icon = Icons.Default.Phone,
-                    keyboardType = KeyboardType.Phone,
+                    icon = Icons.Default.Email,
+                    keyboardType = KeyboardType.Email,
                 )
                 FormInputField(
-                    label = "Mother Phone No",
+                    label = "Phone",
                     onTextChanged = {},
                     icon = Icons.Default.Phone,
-                    keyboardType = KeyboardType.Phone,
-                )
-            }
-
-            if (registerAsTeacher) {
-                FormInputField(
-                    label = "NID",
-                    onTextChanged = {},
-                    icon = Icons.Default.Info,
                     keyboardType = KeyboardType.Phone,
                 )
                 ExposedDropdownMenu(
-                    items = FormData.dept,
+                    items = FormData.bloodGroupList,
+                    onItemSelected = {
+                    },
+                    label = "Blood Group",
+                    icon = Icons.Default.Bloodtype
+                )
+                ExposedDropdownMenu(
+                    items = FormData.genderList,
+                    onItemSelected = {
+                    },
+                    label = "Gender",
+                    icon = Icons.Default.Man
+                )
+                //017
+                //use visual transfer for date of birth
+                //use the date picker for date of birth
+                CustomDatePicker(
+                    label = "Date of Birth",
+                    icon = Icons.Default.DateRange,
+                )
+                ExposedDropdownMenu(
+                    items = FormData.registrationType,
+                    onItemSelected = {
+                        registerAsStudent = it == FormData.registrationType[0]
+                        registerAsTeacher = it == FormData.registrationType[1]
+                    },
+                    label = "Register As",
+                    icon = Icons.Default.AppRegistration
+                )
+
+                if (registerAsStudent) {
+                    FormInputField(
+                        label = "Father Name",
+                        onTextChanged = {
+
+                        },
+                        icon = Icons.Default.Person,
+                    )
+                    FormInputField(
+                        label = "Mother Name",
+                        onTextChanged = {},
+                        icon = Icons.Default.Person,
+                    )
+                    FormInputField(
+                        label = "Father Phone No",
+                        onTextChanged = {},
+                        icon = Icons.Default.Phone,
+                        keyboardType = KeyboardType.Phone,
+                    )
+                    FormInputField(
+                        label = "Mother Phone No",
+                        onTextChanged = {},
+                        icon = Icons.Default.Phone,
+                        keyboardType = KeyboardType.Phone,
+                    )
+                }
+
+                if (registerAsTeacher) {
+                    FormInputField(
+                        label = "NID",
+                        onTextChanged = {},
+                        icon = Icons.Default.Info,
+                        keyboardType = KeyboardType.Phone,
+                    )
+                    ExposedDropdownMenu(
+                        items = FormData.dept,
+                        onItemSelected = {},
+                        label = "Department",
+                        icon = Icons.Default.Book
+                    )
+                }
+
+                ExposedDropdownMenu(
+                    items = FormData.districts,
                     onItemSelected = {},
-                    label = "Department",
-                    icon = Icons.Default.Book
+                    label = "District",
+                    icon = Icons.Default.LocationCity,
+                )
+                ExposedDropdownMenu(
+                    items = FormData.subDistrict,
+                    onItemSelected = {},
+                    label = "SubDistrict",
+                    icon = Icons.Default.LocationCity,
+                )
+                MapLocationInputField(
+                    label = "Location",
+                    text = location,
+                    onMapIconClick = {
+                        showGoogleMap = true
+                    },
+                    icon = Icons.Default.LocationCity
+                )
+                Button(
+                    onClick = { showDrawingBoard = true }) {
+                    Icon(
+                        imageVector = Icons.Default.Upload,
+                        contentDescription = null
+                    )
+                    Text("Signature")
+                }
+
+
+                MyButton(
+                    onClick = { /*TODO*/ },
+                    label = "Register"
                 )
             }
+            //Google map will be displayed
 
-            ExposedDropdownMenu(
-                items = FormData.districts,
-                onItemSelected = {},
-                label = "District",
-                icon = Icons.Default.LocationCity,
-            )
-            ExposedDropdownMenu(
-                items = FormData.subDistrict,
-                onItemSelected = {},
-                label = "SubDistrict",
-                icon = Icons.Default.LocationCity,
-            )
-            MapLocationInputField(
-                label = "Location",
-                text = location,
-                onMapIconClick = {
-                    showGoogleMap = true
-                },
-                icon = Icons.Default.LocationCity
-            )
-            MyButton(
-                onClick = { /*TODO*/ },
-                label = "Register"
-            )
-        }
-        //Google map will be displayed
         if (showGoogleMap) {
             GoogleMapView(
                 onMapDismiss = {
@@ -217,6 +237,11 @@ fun RegistrationForm(
                     location = "${it.latitude} , ${it.longitude}"
                 }
             )
+        }
+        if(showDrawingBoard){
+            WhiteBoard(modifier=Modifier.matchParentSize(),onDismissButtonClick = {
+                showDrawingBoard=false
+            })
         }
 
     }
