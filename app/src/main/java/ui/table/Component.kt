dbaecd7356b698cc7c3132fb3e971d75e253da50
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-07-05 01:54:07 +0600

    Table Layout updated

--- app/src/main/java/ui/table/Component.kt
+++ app/src/main/java/ui/table/Component.kt
@@ -1,16 +1,14 @@
 package ui.table
 
-import android.util.Log
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.drawText
 import androidx.compose.ui.text.rememberTextMeasurer
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
@@ -54,6 +52,7 @@ data class TableCell(
 ) {
     companion object {
         var height = 0f
+        var padding = 20.dp.value
     }
 }
 
@@ -105,50 +104,108 @@ data class Table(
     val cellPadding: Float = 0f,
 
     ) {
-    var columnsTopLeftX = mutableMapOf<Int, Float>()
-    var rowsTopLeftY = mutableMapOf<Int, Float>()
-    fun getRow(rowNo: Int) = rows[rowNo]
-    fun getCell(row: Int, column: Int) = rows[row].getCell(column)
-    fun getHeight(): Float {
-        val numberOfRows = rows.size
-        return numberOfRows * TableCell.height
-    }
+    private var columnsTopLeftX = mutableMapOf<Int, Float>()
+    private var rowsTopLeftY = mutableMapOf<Int, Float>()
+    var verticalLinesTopLeftCoordinates =
+        mutableMapOf<Int, Pair<Offset, Offset>>()
+    var horizontalLinesTopLeftCoordinates =
+        mutableMapOf<Int, Pair<Offset, Offset>>()
 
-    fun getWidth(): Float {
-        return rows.maxOf { it.getWidth() }
-    }
-
-    fun getColumnMaxWidth(rowNo: Int): Float {
+    private fun getColumnMaxWidth(rowNo: Int): Float {
         val result = table.rows.map { row ->
             row.cells[rowNo]
         }.maxOf { it.width }
         return result
     }
 
-    fun calculateCoordinate() {
-        val columnsTopLeftX = mutableMapOf<Int, Float>()
-        val totalColumns = rows.first().cells.size
-        columnsTopLeftX[0] = 0f
-        for (column in 1 until totalColumns) {
-            val tmp = columnsTopLeftX[column - 1] ?: 0f
-            columnsTopLeftX[column] = tmp + getColumnMaxWidth(column - 1)
-        }
-        this.columnsTopLeftX = columnsTopLeftX
+    private fun calculateRowTopLeftY() {
+        val verticalPadding = 2 * TableCell.padding
         val rowsTopLeftY = mutableMapOf<Int, Float>()
-        val totalRow = rows.size
         rowsTopLeftY[0] = 0f
-        for (row in 1 until totalRow) {
+        for (row in 1 until getNumbersOfRow()) {
             val tmp = rowsTopLeftY[row - 1] ?: 0f
-            rowsTopLeftY[row] = tmp + TableCell.height
+            rowsTopLeftY[row] = tmp + TableCell.height + verticalPadding
         }
         this.rowsTopLeftY = rowsTopLeftY
     }
 
-    fun getCellTopLeftCoordinate(row: Int, col: Int):Offset {
-        val offset = Offset(x = columnsTopLeftX[col] ?: 0f, y = rowsTopLeftY[row] ?: 0f)
-        println("TABLE_CELL::CellCoor $offset")
-        return offset
+    private fun calculateColumnTopLeftX() {
+        val horizontalPadding = 2 * TableCell.padding
+        val columnsTopLeftX = mutableMapOf<Int, Float>()
+        rows.first().cells.size
+        columnsTopLeftX[0] = 0f
+        for (column in 1 until getNumbersOfColumn()) {
+            val tmp = columnsTopLeftX[column - 1] ?: 0f
+
+            columnsTopLeftX[column] =
+                tmp + getColumnMaxWidth(column - 1) + horizontalPadding
+        }
+        this.columnsTopLeftX = columnsTopLeftX
     }
+
+    fun calculateCoordinate() {
+        calculateColumnTopLeftX()
+        calculateRowTopLeftY()
+    }
+
+    fun getCellTopLeftCoordinate(row: Int, col: Int): Offset {
+        return Offset(x = columnsTopLeftX[col] ?: 0f, y = rowsTopLeftY[row] ?: 0f)
+    }
+
+    private fun getTableHeight() =
+        (getNumbersOfRow() * TableCell.height) +
+                (getNumbersOfRow() * 2 * TableCell.padding)
+
+
+    fun calculateVerticalLinesCoordinates() {
+        val endPointY = getTableHeight()
+        val verticalLinesTopLeftCoordinates =
+            mutableMapOf<Int, Pair<Offset, Offset>>()
+        verticalLinesTopLeftCoordinates[0] = Pair(
+            Offset(0f, 0f),
+            Offset(0f, endPointY)
+        )
+        for (col in 0 until getNumbersOfColumn()) {
+            val startPointX = columnsTopLeftX[col] ?: 0f
+            verticalLinesTopLeftCoordinates[col] = Pair(
+                Offset(startPointX, 0f),
+                Offset(startPointX, endPointY)
+            )
+        }
+        this.verticalLinesTopLeftCoordinates = verticalLinesTopLeftCoordinates
+
+    }
+
+    private fun getWidth(): Float {
+        val col = getNumbersOfColumn() - 1
+        //
+        val tmp = columnsTopLeftX[col]?:0f
+        val lastColWidth = getColumnMaxWidth(col)
+        return tmp+lastColWidth
+    }
+
+    fun calculateHorizontalLinesCoordinates() {
+        val endPointX = getWidth()
+        val horizontalLinesTopLeftCoordinates =
+            mutableMapOf<Int, Pair<Offset, Offset>>()
+        horizontalLinesTopLeftCoordinates[0] = Pair(
+            Offset(0f, 0f),
+            Offset(endPointX, 0f)
+        )
+        for (row in 1 until getNumbersOfColumn()) {
+            val startPointY = rowsTopLeftY[row] ?: 0f
+            val y = (row*TableCell.height)+(2*TableCell.padding)
+            horizontalLinesTopLeftCoordinates[row] = Pair(
+                Offset(0f, y),
+                Offset(endPointX, y)
+            )
+        }
+        this.horizontalLinesTopLeftCoordinates = horizontalLinesTopLeftCoordinates
+        println("TABLE_CELL::CellCoor $horizontalLinesTopLeftCoordinates")
+    }
+
+    fun getNumbersOfColumn() = rows.first().cells.size
+    fun getNumbersOfRow() = rows.size
 
 
 }
@@ -188,7 +245,6 @@ val table = Table(
 )
 
 
-
 @Preview
 @Composable
 private fun TableComposable() {
@@ -208,10 +264,10 @@ private fun TableComposable() {
         }
         row
     }
-
-
-
     table.calculateCoordinate()
+    table.calculateVerticalLinesCoordinates()
+    table.calculateHorizontalLinesCoordinates()
+
     Canvas(
         modifier = Modifier
             .padding(10.dp)
@@ -220,15 +276,32 @@ private fun TableComposable() {
         table.rows.forEachIndexed { rowNo, row ->
             row.cells.forEachIndexed { colNo, cell ->
                 drawText(
-                    topLeft = table.getCellTopLeftCoordinate(rowNo,colNo),
+                    topLeft = table.getCellTopLeftCoordinate(rowNo, colNo),
                     textMeasurer = textMeasurer,
                     text = cell.text,
                 )
             }
         }
 
+        table.verticalLinesTopLeftCoordinates.forEach {
+            drawLine(
+                start = it.value.first,
+                end = it.value.second,
+                color = Color.Black
+            )
+        }
+        table.horizontalLinesTopLeftCoordinates.forEach {
+            drawLine(
+                start = it.value.first,
+                end = it.value.second,
+                color = Color.Black
+            )
+        }
+
+
     }
-    }
+
+}
 
 
 
