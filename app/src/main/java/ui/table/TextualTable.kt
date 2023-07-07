6a49b4d04837e995c76505b407bd242f565aafee
Khalekuzzaman <74848657+khalekuzzamancse@users.noreply.github.com>
2023-07-07 11:48:44 +0600

    Common Textual table added

--- app/src/main/java/ui/table/TextualTable.kt
+++ app/src/main/java/ui/table/TextualTable.kt
@@ -1,8 +1,6 @@
 package ui.table
 
-import android.telephony.CellIdentity
 import androidx.compose.foundation.border
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
@@ -11,9 +9,8 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -39,13 +36,13 @@ data class TextualTableRow(
 @Composable
 fun TextualTablePreview() {
 
-    val data= mutableListOf<List<String>>()
-    for (i in 1..40){
-        data.add(listOf("Mr Bean","01","Science","4.56"))
+    val data = mutableListOf<List<String>>()
+    for (i in 1..40) {
+        data.add(listOf("Mr Bean", "01", "Science", "4.56"))
     }
     TextualTable(
         listOf("Name", "Student Id", "Department", "Result"),
-        data =data
+        data = data
     )
 }
 
@@ -70,27 +67,24 @@ fun TextualTable(
     for (i in headerText.indices) {
         headerCells.add(TextualTableCell(headerText[i], headerTextWidths[i]))
     }
-
-    Column(modifier = Modifier
-        .padding(8.dp)
-        .fillMaxWidth()) {
-        //Keep header  non moving
-        //so takes a another column
-        TextualTableRow(row = TextualTableRow(headerCells))
-
-        Column(modifier = Modifier
+    Column(
+        Modifier
+            .padding(8.dp)
             .fillMaxWidth()
-            .verticalScroll(rememberScrollState())
-        //find why can not make it horizontal scrollable
-        ) {
-            data.forEach {rowData->
-                val rowCells = mutableListOf<TextualTableCell>()
-                for (i in rowData.indices) {
-                    rowCells.add(TextualTableCell(rowData[i], headerTextWidths[i]))
+    ) {
+        TextualTableRow(row = TextualTableRow(headerCells))
+        LazyColumn {
+            items(data) {
+                data.forEach { rowData ->
+                    val rowCells = mutableListOf<TextualTableCell>()
+                    for (i in rowData.indices) {
+                        rowCells.add(TextualTableCell(rowData[i], headerTextWidths[i]))
+                    }
+                    TextualTableRow(row = TextualTableRow(rowCells))
                 }
-                TextualTableRow(row = TextualTableRow(rowCells))
             }
         }
+
     }
 
 }
